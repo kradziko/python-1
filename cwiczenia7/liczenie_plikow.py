0c0873cfef06059dce52f73738d829711351ccb2
klipski <Kamil.Lipski@poczta.pl>
2016-11-24 19:40:46 +0100

    Ćwiczenia 7. 24.11.2016r.

--- cwiczenia7/liczenie_plikow.py
+++ cwiczenia7/liczenie_plikow.py
@@ -12,7 +12,7 @@ z przeliczeniem na kB i MB
 from os import *
 
 user_path = raw_input("Podaj sciezke: ")
-# user_path = r'/home/klipski/PycharmProjects/python/cwiczenia7/'
+# user_path = r'/home/klipski/PycharmProjects/python/'
 
 ilosc_plikow = 0
 rozmiar_plikow = 0
@@ -31,11 +31,13 @@ for sciezka, podkatalogi, pliki in walk(user_path):
                                                               path.getsize(path.join(sciezka,x))/1024.,
                                                               path.getsize(path.join(sciezka,x))/1024./1024.)
         rozmiar_plikow += sum(path.getsize(path.join(sciezka, nazwa)) for nazwa in pliki)
+        rozmiar_katalogow += sum(path.getsize(path.join(sciezka, nazwa)) for nazwa in podkatalogi)
         ilosc_plikow += len(pliki)
         ilosc_katalogow += len(podkatalogi)
     except:
         print "nie udalo sie pobrac rozmiaru w: ", sciezka
 
+print "\n"
 print "W katalogu %s znajduje sie:" % user_path
 print "- %i plikow o rozmiarze %f b, %f kB, %f MB" % (ilosc_plikow, rozmiar_plikow,
                                                       rozmiar_plikow / 1024., rozmiar_plikow / 1024. / 1024.)
